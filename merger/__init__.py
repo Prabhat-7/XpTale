35b7cbf490c0e187e2a7de9235d12c923b78ab0a
Prabhat Sitaula <prabhatsitaula@gmail.com>
2026-01-08 11:39:23 +0545

    Fixes the issue and works but the package is not yet in use .

---
+++ merger/__init__.py
@@ -0,0 +1,7 @@
+from .config import Config
+from .console import ConsoleStyle
+from .cmd_runner import CmdRunner
+from .normalizer import Normalizer
+from .converter import Converter
+from .merger import Merger
+from .create import Create
--- merger/merger.py
+++ merger/merger.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from .config import Config
 from .console import ConsoleStyle
 from .normalizer import Normalizer
-from cmd_runner import CmdRunner
-
+from .cmd_runner import CmdRunner
+from .converter import Converter 
 
 printer=ConsoleStyle()
 
@@ -48,49 +48,40 @@ class Merger:
                 # Convert HEIC to JPEG using sips
                     converted_jpeg = Path(Config.CONVERTED_FOLDER) / (file.stem + ".jpeg")
                     printer.print_substep(f"Converting HEIC to JPEG: {file.name} -> {converted_jpeg.name}")
-                    run_cmd(["sips", "-s", "format", "jpeg", str(file), "--out", str(converted_jpeg)])
-                    print_success(f"Converted HEIC to JPEG: {converted_jpeg.name}\n")
+                    CmdRunner.run_cmd(["sips", "-s", "format", "jpeg", str(file), "--out", str(converted_jpeg)])
+                    printer.print_success(f"Converted HEIC to JPEG: {converted_jpeg.name}\n")
                     photo_input = converted_jpeg
                 else:
                     photo_input = file
 
             # Convert photo (JPEG/PNG) to video
-                output_file = Path(config.NORMALIZED_FOLDER) / (file.stem + ".mp4")
-                print_substep(f"Converting photo to video: {photo_input.name}")
-                photo_to_video(photo_input, output_file)
-                processed_files.append(output_file)
-                print_success(f"Converted photo to video: {output_file.name}\n")
+                output_file = Path(Config.NORMALIZED_FOLDER) / (file.stem + ".mp4")
+                printer.print_substep(f"Converting photo to video: {photo_input.name}")
+                Converter.convert(photo_input, output_file)
+                self.processed_files.append(output_file)
+                printer.print_success(f"Converted photo to video: {output_file.name}\n")
 
             else:
-                print_warning(f"Skipping unsupported file: {file.name}")
+                printer.print_warning(f"Skipping unsupported file: {file.name}")
 
-
-        # ----------------------------
-        # GENERATE files.txt
-        # ----------------------------
-        print_section("Merging Content")
-        print_step("Generating files.txt for FFmpeg merge...")
-        with open(config.FILES_TXT, "w") as f:
-            for pf in processed_files:
-                f.write(f"file '{pf.as_posix().replace('temp/', '')}'\n")
-        print_success(f"files.txt created at: {config.FILES_TXT}")
+        Merger.create_file()
         # ----------------------------
         # MERGE EVERYTHING
         # ----------------------------
-        print_step("Merging all videos into final output...")
+        printer.print_step("Merging all videos into final output...")
         merge_cmd = [
             "ffmpeg", "-y", "-f", "concat", "-safe", "0",
-            "-i", config.FILES_TXT,
-            "-c:v", config.VIDEO_CODEC, "-preset", config.PRESET, "-crf", config.CRF,
-            "-c:a", config.AUDIO_CODEC,
-            config.OUTPUT_FILE
+            "-i", Config.FILES_TXT,
+            "-c:v", Config.VIDEO_CODEC, "-preset", Config.PRESET, "-crf", Config.CRF,
+            "-c:a", Config.AUDIO_CODEC,
+            Config.OUTPUT_FILE
         ]
-        run_cmd(merge_cmd)
+        CmdRunner.run_cmd(merge_cmd)
 
-        print_section("Task Completed")
-        print_success(f"Merge complete! Final video saved at: [bold]{config.OUTPUT_FILE}[/bold]")
+        printer.print_section("Task Completed")
+        printer.print_success(f"Merge complete! Final video saved at: [bold]{Config.OUTPUT_FILE}[/bold]")
 
         delete_temp_cmd=[
             "rm", "-rf","temp"
         ]
-        run_cmd(delete_temp_cmd)
+        CmdRunner.run_cmd(delete_temp_cmd)
